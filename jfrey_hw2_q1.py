cdfaefeaec6c26ab2086865d2dbca2049936cf0c
jmfrey <jfrey@clarku.edu>
2019-01-31 22:17:50 -0500

    homework 2 answers

--- jfrey_hw2_q1.py
+++ jfrey_hw2_q1.py
@@ -1,10 +1,30 @@
+"""
+Jordan Frey
+Python Programming
+Homework 2, Question 1
+"""
+
+'''
+Tests whether one integer is divisible by another. The user will be prompted to enter two numbers
+(a numerator and a denominator). If the first number is evenly divisible by the second
+number, the program returns the Boolean value True. If not, the program returns the value False.
+'''
+
+
+# Create function divisible_truefalse:
+
+
 def divisible_truefalse():
+    # ask user input for numerator and denominator variables
     numerator = float(raw_input("Input numerator: "))
     denominator = float(raw_input("Input denominator: "))
 
+    # if input is evenly divisible, return true
     if (float(numerator) / float(denominator)) == int(float(numerator) / float(denominator)):
         return True
     else:
         return False
 
+
+divisible_truefalse()
 
--- jfrey_hw2_q2.py
+++ jfrey_hw2_q2.py
@@ -1,11 +1,36 @@
-def feel_temp():
-    x = raw_input("Enter a temperature in degrees celsius: ")
-    if x > 35:
-        return "Hot"
-    elif x >= 20:
-        return "Warm"
-    elif x >= 10:
-        return "Cool"
-    else:
-        return "Cold"
+"""
+Jordan Frey
+Python Programming
+Homework 2, Question 1
+"""
 
+
+"""
+Write a program with a function called feel_temp(). The function
+converts a given temperature in Celsius into four categorical levels- hot,
+warm, cool, and ocld using a set criteria. The program must promp the
+user to input a temperature.
+"""
+
+# Create feel_temp() function
+
+
+def feel_temp():
+    # input an integer for degrees celsius, coerce to integer,
+    # and if invalid number return error.
+    try:
+        x = int(raw_input("Enter a temperature in degrees celsius: "))
+        # Conditional statements
+        if x > 35:
+            return "Hot"
+        elif x >= 20:
+            return "Warm"
+        elif x >= 10:
+            return "Cool"
+        else:
+            return "Cold"
+    except ValueError:
+        print "Please enter a valid number."
+
+
+feel_temp()
